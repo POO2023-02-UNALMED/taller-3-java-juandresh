1b458a74b02e9e70de43395536b28cc053ee129e
juandresh <141792824+juandresh@users.noreply.github.com>
2023-08-26 17:45:30 -0500

    Update TV.java

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -61,18 +61,18 @@ public class TV {
 	
 	public void volumenUp() {
 		if (estado && volumen<7 && volumen>=0) {
-			volumen+=1;}
+			volumen++;}
 	}
 	public void volumenDown() {
 		if (estado && volumen>0 && volumen<=7) {
-			volumen-=1;}
+			volumen--;}
 	}
 	public void canalUp() {
 		if (estado && canal<120 && canal>=1) {
-			canal+=1;}
+			canal++;}
 	}
 	public void canalDown() {
 		if (estado && canal>1 && canal<=120) {
-			canal-=1;}
+			canal--;}
 	}
 }
