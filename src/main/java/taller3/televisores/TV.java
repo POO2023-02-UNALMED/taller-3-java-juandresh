3ae5d8107c65666d53503125de31fa0246921fc6
juandresh <141792824+juandresh@users.noreply.github.com>
2023-08-26 18:23:50 -0500

    Update TV.java

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -20,9 +20,8 @@ public class TV {
 	}
 	
 	public void setCanal(int canal) {
-		if (canal<=1 && canal<=120) {
 		this.canal=canal;}
-	}
+	
 	
 	public int getCanal() {
 		return canal;}
@@ -43,9 +42,7 @@ public class TV {
 		return precio;}
 	
 	public void setVolumen(int volumen) {
-		if(volumen>=0 && volumen<=7) {
 		this.volumen=volumen;}
-	}
 	
 	public int getVolumen() {
 		return volumen;}
